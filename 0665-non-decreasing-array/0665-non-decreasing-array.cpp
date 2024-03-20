5a72bcb0aeaa104784694d0df03ff0661c06061d
Youssef Mohamed <89710015+TURBO70@users.noreply.github.com>
2024-03-20 23:23:47 +0200

    Time: 8 ms (98.77%), Space: 29.5 MB (50.77%) - LeetHub

---
+++ 0665-non-decreasing-array/0665-non-decreasing-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& v) {
+        int cnt=0,n=v.size();
+        bool f=false;
+        for(int i=1;i<n;i++){
+           if(v[i-1]>v[i]){
+            if (f)return false;
+
+            f^=1;
+
+            if(i<2||v[i-2]<=v[i]){
+                v[i-1]=v[i];
+            }else{
+                v[i]=v[i-1];
+            }
+
+
+
+
+           }
+
+
+        }
+        return true;
+    }
+};
