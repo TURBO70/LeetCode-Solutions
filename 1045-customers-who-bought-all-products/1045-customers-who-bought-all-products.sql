ec7c6f0a0c9e9a91f729adb3eb5be61e89b0713d
Youssef Mohamed <89710015+TURBO70@users.noreply.github.com>
2024-08-30 07:02:22 +0300

    Time: 557 ms (56.38%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -3,7 +3,7 @@ select customer_id
 -- on c.product_key =p.product_key 
 from Customer 
 group by customer_id
-having count(distinct product_id)=(
+having count(distinct product_key)=(
     select count(*)
     from Product 
 )
