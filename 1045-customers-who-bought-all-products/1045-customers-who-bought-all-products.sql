f9dd853f34ad724c7a5e60a2b3c380aff5f1527d
Youssef Mohamed <89710015+TURBO70@users.noreply.github.com>
2024-08-30 07:00:57 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -0,0 +1,10 @@
+select customer_id 
+-- from Customer c left join Product p
+-- on c.product_key =p.product_key 
+from Customer 
+group by customer_id
+having count(distinct product_id)=(
+    select count(*)
+    from Product 
+)
+order by customer_id
