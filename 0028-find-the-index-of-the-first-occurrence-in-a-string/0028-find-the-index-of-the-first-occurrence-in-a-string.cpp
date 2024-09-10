3f33511647e8b9dae47776a88e3aeb9d8eba6f36
Youssef Mohamed <89710015+TURBO70@users.noreply.github.com>
2024-09-10 05:03:41 +0300

    Time: 0 ms (100%), Space: 7.7 MB (88.35%) - LeetHub

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if(haystack.find(needle)!=-1){
+            return haystack.find(needle);
+        }else {
+            return -1;
+        }
+    }
+};
